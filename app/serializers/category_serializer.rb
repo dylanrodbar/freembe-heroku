362bbfd0714582efed4c2118c60a2a1847c7e2a3
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-08 17:23:12 -0600

    aaas

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,13 +1,13 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :photo
-  #has_many :subcategory_x_category
+  has_many :subcategory_x_category
 
-  #class SubcategoryXCategorySerializer < ActiveModel::Serializer
-  #  attributes :id, :subcategory_id
-  #  belongs_to :subcategory
+  class SubcategoryXCategorySerializer < ActiveModel::Serializer
+    attributes :id, :subcategory_id
+    belongs_to :subcategory
 
-  #  class SubcategorySerializer < ActiveModel::Serializer
-  #    attributes :id, :name, :description, :photo
-  #  end
-  #end
+    class SubcategorySerializer < ActiveModel::Serializer
+      attributes :id, :name, :description, :photo
+    end
+  end
 end
