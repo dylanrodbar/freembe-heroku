96c2b2905cd7d7c0099a94858c9f6305c1031db4
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-08 17:42:01 -0600

    aasas

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -3,6 +3,7 @@ class CategorySerializer < ActiveModel::Serializer
   has_many :subcategory_x_category, key: "subcategories"
 
   class SubcategoryXCategorySerializer < ActiveModel::Serializer
+    attributes :subcategory_id
     #belongs_to :subcategory
     #class SubcategorySerializer < ActiveModel::Serializer
     #  attributes :id, :name, :description, :photo
