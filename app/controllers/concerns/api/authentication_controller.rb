40df5620880638c0b81a8a0a4802c5eb1c7774f5
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-07 22:28:45 -0600

    as

--- app/controllers/concerns/api/authentication_controller.rb
+++ app/controllers/concerns/api/authentication_controller.rb
@@ -1,17 +1,17 @@
 module Api
 class AuthenticationController < ApplicationController
 
-  #skip_before_action :authenticate_request
-  #def authenticate
-  #  command = AuthenticateUser.call(params[:email], params[:password])
+  skip_before_action :authenticate_request, raise: false
+  def authenticate
+    command = AuthenticateUser.call(params[:email], params[:password])
 
 
-  #  if command.success?
-  #    render json: { auth_token: command.result[0], name: command.result[1], email: command.result[2], photo: command.result[3] }
-  #  else
-  #    render json: { error: command.errors }, status: :unauthorized
-  #  end
-  #end
+    if command.success?
+      render json: { auth_token: command.result[0], name: command.result[1], email: command.result[2], photo: command.result[3] }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
 
 
 end
