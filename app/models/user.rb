87fbd7424f854521b5d9f703f5483deb92c68249
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-07 22:04:53 -0600

    as

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password #método para validar que la contraseña que se brinde sea segura
+  #has_secure_password #método para validar que la contraseña que se brinde sea segura
   has_many :telephones
   has_many :emails
   has_many :announcements
